a9c84835b2112dc1a94f13528dec51ee3ddae2e9
whitw <shkim7876@naver.com>
2021-01-14 23:32:35 +0900

    [base]othello rule:flip

--- othello_solver/main.cpp
+++ othello_solver/main.cpp
@@ -39,7 +39,7 @@ void drawBoard(piece** board, int sz = szboard) {
         for (int j = 0; j < sz; j++) {
             switch (board[i][j]) {
             case piece::EMPTY:
-                printf("бр");
+                printf(". ");
                 break;
             case piece::BLACK:
                 printf("б█");
@@ -119,33 +119,59 @@ void placeAt(piece** board, int x, int y, piece pc, int sz = szboard) {
     if (x < 0 || y < 0 || x >= szboard || y >= szboard || board[x][y] != piece::EMPTY) {
         return;
     }
+
+    //place it anyway.
     board[x][y] = pc;
+    
+    //if the piece was BLACK or WHITE piece, flip pieces by the rule of the othello.
+    if (pc == piece::BLACK || pc == piece::WHITE) {
+        
+        vector<pair<int, int>> toFlip;
+        piece mine = pc;
+        piece yours = (pc == piece::BLACK ? piece::WHITE : piece::BLACK);
+        
+        for (auto dir : dirList) {
+            toFlip.clear();
+            pair<int, int> currPos = make_pair(x, y) + dir;
+            while (boardAt(board,currPos) == yours) {
+                toFlip.push_back(currPos);
+                currPos = currPos + dir;
+            }
+            if (boardAt(board, currPos) == mine) {
+                for (auto flips : toFlip) {
+                    board[flips.first][flips.second] = mine;
+                }
+            }
+        }
+
+    }
 }
 
+bool calcPlaceableCell(piece** board, bool blackTurn, int x, int y, int sz = szboard) {
+    if (boardAt(board, x, y) == piece::EMPTY) {
+        for (auto dir : dirList) {
+            pair<int, int> currPos = make_pair(x, y) + dir;
+            //black turn:pattern W^nB
+            //white turn:pattern B^nW
+            piece mine = blackTurn ? piece::BLACK : piece::WHITE;
+            piece your = blackTurn ? piece::WHITE : piece::BLACK;
+            if (boardAt(board, currPos) == your) {
+                do { currPos = currPos + dir; } while (boardAt(board, currPos) == your);
+                if (boardAt(board, currPos) == mine) {
+                    return true;
+                }
+            }
+        }
+    }
+    return false;
+}
 vector<pair<int, int>> calcPlaceable(piece** board, bool blackTurn, int sz=szboard) {
     vector<pair<int, int>> result;
     for (int i = 0; i < sz;i++) {
         for (int j = 0; j < sz; j++) {
-
-            //check for board[i][j]
-            if (board[i][j] == piece::EMPTY) {
-                for (auto dir:dirList) {
-                    pair<int, int> currPos = make_pair(i,j) + dir;
-                    //black turn:pattern W^nB
-                    //white turn:pattern B^nW
-                    piece mine = blackTurn ? piece::BLACK : piece::WHITE;
-                    piece your = blackTurn ? piece::WHITE : piece::BLACK;
-                    if (boardAt(board, currPos) == your) {
-                        do { currPos = currPos + dir; } while (boardAt(board, currPos) == your);
-                        if (boardAt(board, currPos) == mine) {
-                            result.push_back(make_pair(i,j));
-                            //board[i][j] is definitely placable; no need to calculate other direction anymore.
-                            break;
-                        }
-                    }
-                }
+            if (calcPlaceableCell(board, blackTurn, i, j, sz)) {
+                result.push_back(make_pair(i, j));
             }
-
         }
     }
     return result;
@@ -163,10 +189,27 @@ int main() {
         board[i] = new piece[szboard];
     }
     init(board);
+    for (int i = 0; i < 5; i++) {
+        drawBoard(board);
+        cout << "select the position to place obstacles(" << i + 1 << "/5):";
+        while (true) {
+            cin >> a >> b;
+            if (cin.fail()) {
+                cin.clear();
+                cin.ignore(256, '\n');
+                continue;
+            }
+            else {
+                break;
+            }
+        }
+        placeAt(board, a, b, piece::OBSTACLE);
+    }
     bool blackTurn = blackFirst;
     vector<pair<int, int>> placeAble;
     while(true){
-        drawPlaceAble(board, blackTurn);
+        drawBoard(board);
+        //drawPlaceAble(board, blackTurn);
         cout << "Turn:" << (blackTurn ? "BLACK" : "WHITE") << endl;
         cout << "placable:";
         placeAble = calcPlaceable(board, blackTurn);
@@ -183,6 +226,8 @@ int main() {
             }
             //if (a,b) not in placeAble continue;
             bool _valid = false;
+            if (placeAble.size() == 0)
+                _valid = true;
             for (auto i : placeAble) {
                 if (make_pair(a, b) == i) {
                     _valid = true;
